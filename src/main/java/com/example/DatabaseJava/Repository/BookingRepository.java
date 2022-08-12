6ba899f29e9c4cf92ad8f2661d97c7e2b8f0f3f3
sackboynick <nicola.santolini@live.com>
2022-08-12 03:28:42 +0200

    Updates

--- src/main/java/com/example/DatabaseJava/Repository/BookingRepository.java
+++ src/main/java/com/example/DatabaseJava/Repository/BookingRepository.java
@@ -3,14 +3,16 @@ package com.example.DatabaseJava.Repository;
 import com.example.DatabaseJava.model.Booking;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 
 import javax.transaction.Transactional;
+import java.sql.SQLException;
 import java.util.List;
 @Repository
 public interface BookingRepository extends CrudRepository<Booking, Integer>
 {
-  @Transactional
-  void deleteById(int id);
+    @Transactional(rollbackOn={SQLException.class} )
+    void deleteById(int id);
   List<Booking> findById(int id);
  /* List<Booking> findByName(String name);*/
   List<Booking> findByFlightNo(int FlightNO);
